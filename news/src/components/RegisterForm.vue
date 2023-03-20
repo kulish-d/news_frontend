2a61e2b5fa8f21f1668a1faf7b7b9709010630cc
d.kulish <d.kulish@tg.dunice.net>
2023-03-20 15:25:57 +0300

    small things

--- news/src/components/RegisterForm.vue
+++ news/src/components/RegisterForm.vue
@@ -1,77 +1,84 @@
 <template>
-    <v-row justify="center">
-      <v-dialog
-        v-model="dialog"
-        persistent
-        width="1024"
-      >
-        <template v-slot:activator="{ on, attrs }">
+  <v-row justify="center">
+    <v-dialog
+      v-model="dialog"
+      persistent
+      width="1024"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          id="head-btn"
+          color="purple darken-1"
+          elevation="15"
+          large
+          v-bind="attrs"
+          v-on="on"
+        >
+          Sign Up
+        </v-btn>
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="text-h5">Registration</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="3">
+                <v-text-field
+                  label="Username"
+                  required
+                >
+                
+                </v-text-field>
+              </v-col>
+              <v-col cols="9">
+                <v-text-field
+                  label="Email"
+                  required
+                >
+                
+                </v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                  label="Password"
+                  type="password"
+                  required
+                >
+                
+                </v-text-field>
+                <v-text-field
+                  label="Password (again)"
+                  type="password"
+                  required
+                >
+                </v-text-field>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
           <v-btn
-            id="head-btn"
-            color="purple darken-1"
-            elevation="15"
-            large
-            v-bind="attrs"
-            v-on="on"
+            color="blue-darken-1"
+            variant="text"
+            @click="dialog = false"
           >
-            Register
+            Close
           </v-btn>
-        </template>
-        <v-card>
-          <v-card-title>
-            <span class="text-h5">User Profile</span>
-          </v-card-title>
-          <v-card-text>
-            <v-container>
-              <v-row>
-                <v-col cols="3">
-                  <v-text-field
-                    label="Username"
-                    required
-                  ></v-text-field>
-                </v-col>
-                <v-col cols="9">
-                  <v-text-field
-                    label="Email"
-                    required
-                  ></v-text-field>
-                </v-col>
-                <v-col cols="12">
-                  <v-text-field
-                    label="Password"
-                    type="password"
-                    required
-                  ></v-text-field>
-                  <v-text-field
-                    label="Password (again)"
-                    type="password"
-                    required
-                  ></v-text-field>
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn
-              color="blue-darken-1"
-              variant="text"
-              @click="dialog = false"
-            >
-              Close
-            </v-btn>
-            <v-btn
-              color="blue-darken-1"
-              variant="text"
-              @click="dialog = false"
-            >
-              Save
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-    </v-row>
-  </template>
+          <v-btn
+            color="blue-darken-1"
+            variant="text"
+            @click="dialog = false"
+          >
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
 
 
 <script>
@@ -79,9 +86,20 @@ export default {
   name: 'RegisterForm',
   components: {
   },
+
+  mounted:  {
+    attrsPrint() {
+      console.log();
+      return this.Username;
+    },
+  },
+  
+
   data: () => ({
     dialog: false,
+    Username: null,
   }),
+
 }
 </script>
 
