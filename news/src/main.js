32ce87dfc88b39459a0dd2cd0819d68911ca3f6d
d.kulish <d.kulish@tg.dunice.net>
2023-04-11 18:02:05 +0300

    start

--- news/src/main.js
+++ news/src/main.js
@@ -4,8 +4,11 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import store from './store'
 import router from './plugins/router'
+import GSignInButton from 'vue-google-signin-button'
 
 Vue.config.productionTip = false
+
+Vue.use(GSignInButton)
 
 new Vue({
   vuetify,
